549c26466ef161513159aabc7cae78a70b24debb
Nikolas Namikas <nick.namikas@incentify.com>
2023-09-26 09:49:53 -0700

    Test

---
+++ main.go
@@ -0,0 +1,18 @@
+package stringutils
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Upper(x string) string() {
+
+	return strings.ToUpper(x)
+
+}
+
+func Lower(x string) string() {
+
+	return strings.ToLower(x)
+
+}
